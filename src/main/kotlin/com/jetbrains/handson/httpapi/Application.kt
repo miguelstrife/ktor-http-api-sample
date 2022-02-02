fb264ba0cb9d1727cff8128cbb6066168c002958
Mike Fajardo Turner <miguelstrife@gmail.com>
2022-02-01 21:10:27 -0600

    Initial modifications of Ktor API project
    Initial modifications of Ktor API project

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,25 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
+import routes.customerRouting
+import routes.registerOrderRoutes
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+    this.registerCustomerRoutes()
+}
 
+fun Application.registerCustomerRoutes() {
+    routing {
+        customerRouting()
+        registerOrderRoutes()
+    }
 }
